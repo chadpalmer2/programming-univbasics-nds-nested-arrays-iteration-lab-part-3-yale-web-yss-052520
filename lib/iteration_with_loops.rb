96057e6dd213312b472acf850bd8eac49dfd65e9
Chad Palmer <chad.palmer@yale.edu>
2020-05-15 19:48:21 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -14,3 +14,11 @@ def join_nested_strings(src)
   
   string_arr.join(" ")
 end
+
+mixed_data = [
+  ["The", 4, "quick"],
+  [-1, "brown", "fox", 30],
+  ["studied", 101, 233, "Ruby"]
+]
+ 
+join_nested_strings(mixed_data)
